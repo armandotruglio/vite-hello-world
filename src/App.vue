0227cfa7d07490bf3f41f39c82a77bf8c96d1799
armandotruglio <armando.truglio1@gmail.com>
2024-09-13 18:07:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <AppTitle />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
