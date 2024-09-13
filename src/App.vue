7fbd38bbdc180caf467c323802903c7a45ac7ab3
armandotruglio <armando.truglio1@gmail.com>
2024-09-13 18:16:00 +0200

    Added Jumbotron, esercizio e bonus finiti

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   components: {
-    AppTitle
+    AppTitle,
+    AppHero
   },
   data() {
     return {
@@ -16,6 +18,9 @@ export default {
 <template>
   <div>
     <AppTitle />
+  </div>
+  <div>
+    <AppHero />
   </div>
 </template>
 
---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<template>
+    <h2>
+        Questo è un JUMBOTRON!!
+    </h2>
+</template>
